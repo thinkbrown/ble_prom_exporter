0816b99c46b88abd62f689f3b604ecbd074c14d0
Logan Brown <thinkbrown@96west.net>
2021-01-03 10:57:06 -0500

    so many features. Logging with timestamps, clean shutdowns with signal handling

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ import socketserver
 import http.server
 import threading
 import sys
-
+import signal
+import datetime
 config = configparser.ConfigParser()
 config.read('/etc/ble_exporter.conf')
 dataPoints = {}
+server = None
+scanner = None
 
+def logger(message):
+    print(f"[{datetime.datetime.now().ctime()}] {message}", flush=True)
 
 class updateData(bluepy.btle.DefaultDelegate):
     def __init__(self):
@@ -19,7 +24,7 @@ class updateData(bluepy.btle.DefaultDelegate):
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if dev.addr in config.sections():
             dev_name = config[dev.addr]['name']
-            print(f"Received new data from {dev_name}")
+            logger(f"Received new data from {dev_name}")
             data = dev.getValueText(22)
             temperature = int(data[16:20], 16)/10
             humidity = int(data[20:22], 16)
@@ -46,18 +51,42 @@ class metricHandler(http.server.SimpleHTTPRequestHandler):
         self.wfile.write(bytes(buffer, "utf8"))
         return
 
-def runServer():
-    global dataPoints
-    with socketserver.TCPServer(("", int(config['global']['port'])), metricHandler) as httpd:
-        httpd.allow_reuse_address = True
-        try:
-            print(f"Server started at localhost:{config['global']['port']}")
-            httpd.serve_forever()
-        except:
-            httpd.server_close()
 
-daemon = threading.Thread(name="metricServer", target=runServer)
-daemon.setDaemon(True)
-daemon.start()
-scanner = bluepy.btle.Scanner().withDelegate(updateData())
-scanner.scan(timeout = 0)
+class metricServer(threading.Thread):
+    def run(self):
+        global dataPoints
+        self.httpd = socketserver.TCPServer(("", int(config['global']['port'])), metricHandler)
+        logger(f"Server started at localhost:{config['global']['port']}")
+        self.httpd.serve_forever()
+    def stop(self):
+        self.httpd.server_close
+        logger("metricServer closed")
+
+
+
+
+def main():
+    global server
+    global scanner
+    shutdownEvent = threading.Event()
+    server = metricServer()
+    server.start()
+    scanner = bluepy.btle.Scanner().withDelegate(updateData())
+    scanner.start()
+    while True:
+        scanner.process(timeout = 3)
+
+
+def shutdownHandler(sig, frame):
+    logger('Signal received, shutting down...')
+    global server
+    global scanner
+    server.stop()
+    scanner.stop()
+    logger("scanner stopped, we're done here")
+    sys.exit(0)
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, shutdownHandler)
+    signal.signal(signal.SIGTERM, shutdownHandler)
+    main()
